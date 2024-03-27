327ef7b61a20dc1d0e4111a651d3b261f76c7003
shiro76 <97417973+shiro76@users.noreply.github.com>
2024-03-27 05:35:15 +0100

    Update 3_data-prep.py

--- 3_data-prep.py
+++ 3_data-prep.py
@@ -1,5 +1,7 @@
+# Importer les bibliothèques nécessaires
 from unsloth import FastLanguageModel
-
+from transformers import AutoTokenizer
+from datasets import load_dataset
 #data prep
 alpaca_prompt = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
 
