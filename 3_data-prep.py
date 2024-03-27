a43972238692c13a3bc4fc685373ab0628f269e1
shiro76 <97417973+shiro76@users.noreply.github.com>
2024-03-27 05:33:41 +0100

    Update 3_data-prep.py

--- 3_data-prep.py
+++ 3_data-prep.py
@@ -1,3 +1,5 @@
+from unsloth import FastLanguageModel
+
 #data prep
 alpaca_prompt = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
 
